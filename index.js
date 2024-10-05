b9cee5e3b6dbb516a792bfc6ce836e59c854c1bc
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-04 21:18:12 -0400

    fdf

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+const PORT = 3000;
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+//Database Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'messages'})
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello Worlds");
 })
 
-app.listen(3000, () => {
+//create a new message
+app.post('/message', (req, res)=> {
+    const {name, email, message} = req.body;
+    const query = `INSERT INTO message (name, email, message) VALUES ('${name}', '${email}', '${message}')`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send('Message created');
+    });
+})
+
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
